31045633927a813f1d03c3f941fb35d0e7dbafc4
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-03-19 14:48:08 +0100

    Update example

--- example.py
+++ example.py
@@ -1,12 +1,11 @@
 import maildropper
-m = Maildropper('user', 'pwd', 'example.com')  # IMAP over SSL
+with Maildropper('user', 'pwd', 'example.com') as m:  # IMAP over SSL
+    # script will exit after m.drop is called
 
-# script will exit after m.drop is called
-
-# Mailing Lists
-if m.header('Precedence').startswith('list'):
-    if 'notifications@github.com' in m.header('From'):
-        if 'markus@unterwaditzer.net' in m.header('Cc'):
-            m.drop('INBOX.ml.github', flagged=True)
-        m.drop('INBOX.ml.github')
-    m.drop('INBOX.ml')
+    # Mailing Lists
+    if m.header('Precedence').startswith('list'):
+        if 'notifications@github.com' in m.header('From'):
+            if 'markus@unterwaditzer.net' in m.header('Cc'):
+                m.drop('INBOX.ml.github', flagged=True)
+            m.drop('INBOX.ml.github')
+        m.drop('INBOX.ml')
